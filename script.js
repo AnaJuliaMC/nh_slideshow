99007eabca2232d978d2fd3df7953c6efde86f2e
Ana Julia Menegasso <130059630+AnaJuliaMC@users.noreply.github.com>
2024-04-05 18:51:16 -0300

    .

---
+++ script.js
@@ -0,0 +1,22 @@
+const slide = document.querySelector('.slide');
+const imagens = [
+    './imagens/img1.jpeg',
+    './imagens/img2.jpeg',
+    './imagens/img3.jpeg'
+]
+
+var time = 1000
+var count = 0
+
+function passarSlide(){
+    slide.src = imagens[count]
+    if(count < imagens.length - 1){
+        count++;
+    }
+    else{
+        count = 0
+    }
+     setTimeout('passarSlide()', time)
+}
+
+window.onload = passarSlide
